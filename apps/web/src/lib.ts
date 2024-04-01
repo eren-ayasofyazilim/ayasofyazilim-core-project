f21d7b095a95f079d684efc419d7e22bc155ee42
Eren <eren.odaci@ayasofyazilim.com>
2024-04-01 11:31:26 +0300

    fix: set cookies for login

--- apps/web/src/lib.ts
+++ apps/web/src/lib.ts
@@ -3,12 +3,11 @@ import { cookies } from "next/headers";
 
 export function getAppClient(): AppClient {
   const cookieStore = cookies();
-  const cookie = cookieStore.get("Cookie1");
   return new AppClient({
     BASE: "http://192.168.1.37:44399",
     HEADERS: {
       "X-Requested-With": "XMLHttpRequest",
-      Cookie: cookie?.value || "",
+      Cookie: cookieStore.toString(),
     },
   });
 }
