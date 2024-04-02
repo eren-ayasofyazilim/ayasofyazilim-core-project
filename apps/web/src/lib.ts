532d843c5d6daa3ca25d2129c1035dc2f5e55f54
Eren <eren.odaci@ayasofyazilim.com>
2024-04-02 12:01:57 +0300

    refactor:(saas) code changes related to saas update

--- apps/web/src/lib.ts
+++ apps/web/src/lib.ts
@@ -1,9 +1,20 @@
-import { AppClient } from "ayasofyazilim-saas";
+import { AccountServiceClient } from "ayasofyazilim-saas/AccountService";
+import { ProjectServiceClient } from "ayasofyazilim-saas/ProjectService";
 import { cookies } from "next/headers";
 
-export function getAppClient(): AppClient {
+export function getAccountServiceClient(): AccountServiceClient {
   const cookieStore = cookies();
-  return new AppClient({
+  return new AccountServiceClient({
+    BASE: "http://192.168.1.37:44399",
+    HEADERS: {
+      "X-Requested-With": "XMLHttpRequest",
+      Cookie: cookieStore.toString(),
+    },
+  });
+}
+export function getProjectServiceClient(): ProjectServiceClient {
+  const cookieStore = cookies();
+  return new ProjectServiceClient({
     BASE: "http://192.168.1.37:44399",
     HEADERS: {
       "X-Requested-With": "XMLHttpRequest",
