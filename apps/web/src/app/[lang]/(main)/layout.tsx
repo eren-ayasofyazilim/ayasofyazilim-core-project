f673149898ae759dcf6c5b4d3c166fb6a6a78d62
Abdurrahman Rajab <rajab-2@windowslive.com>
2024-04-03 11:37:07 +0300

    feat: add locale layout (#16)
    Add language selector to main layout:
    
    
    ![image](https://github.com/ayasofyazilim-clomerce/ayasofyazilim-core-project/assets/18273833/1630c312-20bd-49fd-a637-2ddf3d7ecea2)

--- apps/web/src/app/[lang]/(main)/layout.tsx
+++ apps/web/src/app/[lang]/(main)/layout.tsx
@@ -8,6 +8,9 @@ import { JSXElementConstructor, ReactElement, ReactNode, useEffect, useState } f
 import Mainlayout, { mainLayoutProps } from "@repo/ayasofyazilim-ui/templates/mainlayout";
 import "./../../globals.css";
 import { Volo_Abp_Account_ProfileDto } from 'ayasofyazilim-saas';
+import { useLocale } from 'src/providers/locale';
+import { CountrySelector } from '@repo/ayasofyazilim-ui/organisms/country-selector';
+import { lang } from '@repo/ayasofyazilim-ui/pages/login';
 
 type LayoutProps = {
     children: ReactElement<any, string | JSXElementConstructor<any>>;
@@ -28,44 +31,50 @@ type Submenu = {
 };
 
 export default function Layout({ children }: LayoutProps) {
+     
+    const { cultureName, resources, changeLocale } = useLocale();
+    let resourcesMap = {
+        profile: resources?.AbpUi?.texts?.PersonalInfo || "Profile",
+        dashboard: resources?.AbpForDeploy?.texts?.["Menu:Dashboard"] || "Dashboard",
+    }
     const router = useRouter();
     const navigationLinks = [
         {
-          href: '/profile',
-          text: 'Profile',
+            href: '/profile',
+            text: resourcesMap.profile,
         },
         {
-          href: '/dashboard',
-          text: 'Dashboard',
+            href: '/dashboard',
+            text: resourcesMap.dashboard,
         },
         {
-          title: 'Pages',
-          submenu: [
-            {
-              title: 'Dashboard',
-              href: '/dashboard',
-              description:
-                'Show the user dashboard.',
-            },
-            {
-              title: 'Profile',
-              href: '/profile',
-              description:
-                'Show the user profile',
-            },
-          ]
+            title: 'Pages',
+            submenu: [
+                {
+                    title: resourcesMap.dashboard,
+                    href: '/dashboard',
+                    description:
+                        'Show the user dashboard.',
+                },
+                {
+                    title: resourcesMap.profile,
+                    href: '/profile',
+                    description:
+                        'Show the user profile',
+                },
+            ]
         }
-      ];
+    ];
     const exampleMenus: MenuProps[] = [
         {
             label: 'Pages',
-            name: 'Profile',
+            name: resourcesMap.profile,
             icon: <User size={15} className="mr-2" />,
             href: 'profile',
         },
         {
             label: 'Pages',
-            name: 'Dashboard',
+            name: resourcesMap.dashboard,
             icon: <SquareStack size={15} className="mr-2" />,
             href: 'dashboard',
         },
@@ -89,18 +98,18 @@ export default function Layout({ children }: LayoutProps) {
         menuLinks: [
             {
                 href: 'profile',
-                text: 'Profile',
+                text: resourcesMap.profile,
                 shortcut: '⌘P',
             },
             {
                 href: 'dashboard',
-                text: 'Dashboard',
+                text: resourcesMap.dashboard,
                 shortcut: '⌘D',
             },
         ],
         logoutFunction: async () => {
-            let result =  await fetch('/api/auth/logout');
-            if(result.status !== 200){
+            let result = await fetch('/api/auth/logout');
+            if (result.status !== 200) {
                 console.error('Failed to logout');
                 return;
             }
@@ -114,6 +123,14 @@ export default function Layout({ children }: LayoutProps) {
             menus={exampleMenus}
             userNav={userNavigation}
             navMenu={navigationLinks}
+            extraMenu={
+                <CountrySelector
+                    menuAlign="end"
+                    countries={lang.countries}
+                    defaultValue={cultureName}
+                    onValueChange={changeLocale}
+                />
+            }
         >
             <>
                 {children}
