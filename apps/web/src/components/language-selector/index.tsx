f47d4660e088b93bc2c58c8d38436ef6902dba15
Ertuğrulcan Bakas <ecbakas@gmail.com>
2024-04-04 11:55:02 +0300

    feat:langugage-selector tooltip

--- apps/web/src/components/language-selector/index.tsx
+++ apps/web/src/components/language-selector/index.tsx
@@ -1,26 +1,40 @@
 "use client";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@repo/ayasofyazilim-ui/atoms/tooltip";
 import { CountrySelector } from "@repo/ayasofyazilim-ui/organisms/country-selector";
 import { useContext, useEffect } from "react";
 import { LocaleContext } from "src/providers/locale";
 import countries from "./data";
 
-export default function LanguageSelector(): JSX.Element {
+export default function LanguageSelector({
+  menuAlign,
+}: {
+  menuAlign?: "start" | "center" | "end";
+}): JSX.Element {
   const { cultureName, resources, changeLocale } = useContext(LocaleContext);
-  useEffect(() => {
-    console.log(cultureName, resources);
-  }, [cultureName]);
+  useEffect(() => {}, [cultureName]);
   return (
-    <div>
-      <CountrySelector
-        searchText="Find"
-        searchEmptyValue="No country found."
-        defaultValue={cultureName}
-        countries={countries}
-        onValueChange={(value: string) => {
-          changeLocale(value);
-        }}
-      />
-      {resources?.AbpUi.texts?.Welcome}
-    </div>
+    <Tooltip>
+      <TooltipTrigger asChild>
+        <div className="flex">
+          <CountrySelector
+            searchText={resources?.AbpUi.texts?.Search}
+            searchEmptyValue={
+              resources?.AbpExceptionHandling?.texts?.DefaultErrorMessage404
+            }
+            menuAlign={menuAlign}
+            defaultValue={cultureName}
+            countries={countries}
+            onValueChange={(value: string) => {
+              changeLocale(value);
+            }}
+          />
+        </div>
+      </TooltipTrigger>
+      <TooltipContent>{resources?.AbpUi?.texts?.Language}</TooltipContent>
+    </Tooltip>
   );
 }
