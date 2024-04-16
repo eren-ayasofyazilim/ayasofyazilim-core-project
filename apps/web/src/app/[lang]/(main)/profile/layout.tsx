17a5d0afaf29f5c5106082d3ea7b0ff164724f4a
Eren <eren.odaci@ayasofyazilim.com>
2024-04-16 14:49:46 +0300

    style: minor change

--- apps/web/src/app/[lang]/(main)/profile/layout.tsx
+++ apps/web/src/app/[lang]/(main)/profile/layout.tsx
@@ -32,10 +32,7 @@ export default function Layout({ children }: LayoutProps) {
   }, []);
   return (
     <div className="mx-auto grid w-full max-w-6xl items-start gap-6 md:grid-cols-[180px_1fr] lg:grid-cols-[250px_1fr]">
-      <nav
-        className="grid gap-4 text-sm text-muted-foreground"
-        x-chunk="dashboard-04-chunk-0"
-      >
+      <nav className="grid gap-4 text-sm text-muted-foreground">
         {navbarItems.map((item) => (
           <Link
             href={"/profile/" + (item.path ?? "")}
