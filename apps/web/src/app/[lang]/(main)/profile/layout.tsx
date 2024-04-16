b6694d49d48633267580408d876cad09d9b31d22
Eren <eren.odaci@ayasofyazilim.com>
2024-04-16 14:27:42 +0300

    style: unnecessary code removed

--- apps/web/src/app/[lang]/(main)/profile/layout.tsx
+++ apps/web/src/app/[lang]/(main)/profile/layout.tsx
@@ -31,28 +31,26 @@ export default function Layout({ children }: LayoutProps) {
     }
   }, []);
   return (
-    <div className="bg-muted/40 h-screen pt-5">
-      <div className="mx-auto grid w-full max-w-6xl items-start gap-6 md:grid-cols-[180px_1fr] lg:grid-cols-[250px_1fr]">
-        <nav
-          className="grid gap-4 text-sm text-muted-foreground"
-          x-chunk="dashboard-04-chunk-0"
-        >
-          {navbarItems.map((item) => (
-            <Link
-              href={"/profile/" + (item.path ?? "")}
-              className={item.path === path ? "font-semibold text-primary" : ""}
-              key={item.name}
-            >
-              {item.name}
-            </Link>
-          ))}
-          <Link href="#">Integrations</Link>
-          <Link href="#">Support</Link>
-          <Link href="#">Organizations</Link>
-          <Link href="#">Advanced</Link>
-        </nav>
-        <div className="grid gap-6">{children}</div>
-      </div>
+    <div className="mx-auto grid w-full max-w-6xl items-start gap-6 md:grid-cols-[180px_1fr] lg:grid-cols-[250px_1fr]">
+      <nav
+        className="grid gap-4 text-sm text-muted-foreground"
+        x-chunk="dashboard-04-chunk-0"
+      >
+        {navbarItems.map((item) => (
+          <Link
+            href={"/profile/" + (item.path ?? "")}
+            className={item.path === path ? "font-semibold text-primary" : ""}
+            key={item.name}
+          >
+            {item.name}
+          </Link>
+        ))}
+        <Link href="#">Integrations</Link>
+        <Link href="#">Support</Link>
+        <Link href="#">Organizations</Link>
+        <Link href="#">Advanced</Link>
+      </nav>
+      <div className="grid gap-6">{children}</div>
     </div>
   );
 }
