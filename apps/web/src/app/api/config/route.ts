2c92750a18e21679e9815e51bea6159290e93ba7
abdurrahman rajab <abdurrahman.rajab@ayasofyazilim.com>
2024-04-30 13:33:56 +0300

    refactor: remove unused code

--- apps/web/src/app/api/config/route.ts
+++ apps/web/src/app/api/config/route.ts
@@ -1,10 +1,7 @@
 import { getAccountServiceClient } from "src/lib";
-import { auth as nextAuth } from "auth";
 
 export async function GET(request: Request) {
-  const session = await nextAuth();
-  let token = session?.accessToken || "";
-  const client = await getAccountServiceClient(token);
+  const client = await getAccountServiceClient();
   const result =
     await client.abpApplicationConfiguration.getApiAbpApplicationConfiguration(false);
   return new Response(JSON.stringify({ message: result.currentUser }));
