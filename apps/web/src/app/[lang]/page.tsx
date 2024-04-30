5f102ed11a00dcb0e8fd03c7acbf5d7a33de1ebf
abdurrahman rajab <abdurrahman.rajab@ayasofyazilim.com>
2024-04-30 10:40:39 +0300

    chore: remove code from main page

--- apps/web/src/app/[lang]/page.tsx
+++ apps/web/src/app/[lang]/page.tsx
@@ -1,15 +1,11 @@
 "use client";
-import { Button } from "@/components/ui/button";
 import LanguageSelector from "components/language-selector";
-import { signIn } from "next-auth/react";
 import Link from "next/link";
-import { isServerSide } from "src/utils";
 
 export default function Page(): JSX.Element {
   return (
     <div>
       <LanguageSelector />
-
       <Link href={"login"}>Login</Link>
     </div>
   );
