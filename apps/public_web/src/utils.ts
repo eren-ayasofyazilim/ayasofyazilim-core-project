0ab57ccac468c9c68ca4ba9ff53961b3d1b53da2
Eren <eren.odaci@ayasofyazilim.com>
2024-05-07 16:12:13 +0300

    feat: add getBaseLinkWithoutLocale

--- apps/public_web/src/utils.ts
+++ apps/public_web/src/utils.ts
@@ -10,7 +10,7 @@ export function getBaseLink(location: string, locale?: string) {
       const cookieStore = require("next/headers").cookies();
       locale = cookieStore.get("locale")?.value ?? "en";
     }
-    return process.env.PROJECT_BASE_URL + "/" + locale + "/" + location;
+    return getBaseLinkWithoutLocale(locale + "/" + location);
   }
   // it's client side
   if (!locale) {
@@ -18,5 +18,13 @@ export function getBaseLink(location: string, locale?: string) {
     const pathnameParts = pathname.split("/");
     locale = pathnameParts?.[1] ?? "en";
   }
-  return window.location.origin + "/" + locale + "/" + location;
+  return getBaseLinkWithoutLocale(locale + "/" + location);
+}
+export function getBaseLinkWithoutLocale(location: string) {
+  // if it's server side
+  if (isServerSide()) {
+    return process.env.PROJECT_BASE_URL + "/" + location;
+  }
+  // it's client side
+  return window.location.origin + "/" + location;
 }
