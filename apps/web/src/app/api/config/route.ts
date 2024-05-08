130e45be5221468661c768046f07296103d4d664
Eren <eren.odaci@ayasofyazilim.com>
2024-05-08 13:32:44 +0300

    refactor: api request changes

--- apps/web/src/app/api/config/route.ts
+++ apps/web/src/app/api/config/route.ts
@@ -3,6 +3,8 @@ import { getAccountServiceClient } from "src/lib";
 export async function GET(request: Request) {
   const client = await getAccountServiceClient();
   const result =
-    await client.abpApplicationConfiguration.getApiAbpApplicationConfiguration(false);
+    await client.abpApplicationConfiguration.getApiAbpApplicationConfiguration({
+      includeLocalizationResources: false,
+    });
   return new Response(JSON.stringify({ message: result.currentUser }));
 }
