75b3b51ebe16f73055663c6b7a81ae5a656947d0
Eren <eren.odaci@ayasofyazilim.com>
2024-05-08 09:05:12 +0300

    refactor: change api request

--- apps/web/src/app/api/route.ts
+++ apps/web/src/app/api/route.ts
@@ -13,7 +13,7 @@ export async function GET(request: Request) {
   // Use the client instance to make the API call
   const response =
     await appClient.abpApplicationLocalization.getApiAbpApplicationLocalization(
-      lang
+      { cultureName: lang }
     );
 
   // set cookie
