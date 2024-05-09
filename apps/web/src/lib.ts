1f9c37ca4d7aa0a1e40278beb0d2b8cb3c450b98
abdurrahman rajab <abdurrahman.rajab@ayasofyazilim.com>
2024-05-09 13:56:43 +0300

    feat: add identity client

--- apps/web/src/lib.ts
+++ apps/web/src/lib.ts
@@ -1,6 +1,20 @@
 import { AccountServiceClient } from "@ayasofyazilim/saas/AccountService";
 import { ProjectServiceClient } from "@ayasofyazilim/saas/ProjectService";
+import { IdentityServiceClient } from "@ayasofyazilim/saas/IdentityService"
 import { auth } from "auth";
+
+export async function getIdentityServiceClient(): IdentityServiceClient {
+  const session = await auth();
+  const token = session?.accessToken || "";
+  return new IdentityServiceClient({
+    TOKEN: token,
+    BASE: process.env.BASE_URL,
+    HEADERS: {
+      "X-Requested-With": "XMLHttpRequest",
+      "Content-Type": "application/json",
+    },
+  });
+}
 
 export async function getAccountServiceClient(): AccountServiceClient {
   const session = await auth();
