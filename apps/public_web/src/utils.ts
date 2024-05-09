4b4b23170c3324e343d0016c68478ae0d33bd420
abdurrahman rajab <abdurrahman.rajab@ayasofyazilim.com>
2024-05-09 10:38:15 +0300

    refactor: utils purify base link function

--- apps/public_web/src/utils.ts
+++ apps/public_web/src/utils.ts
@@ -2,29 +2,20 @@ export function isServerSide() {
   return typeof window === "undefined";
 }
 
-export function getBaseLink(location: string, locale?: string) {
-  // if it's server side
+function getLocale(locale?: string){
   if (isServerSide()) {
-    // if locale is missing get locale from cookies provided cookie exists
-    if (!locale) {
-      const cookieStore = require("next/headers").cookies();
-      locale = cookieStore.get("locale")?.value ?? "en";
-    }
-    return getBaseLinkWithoutLocale(locale + "/" + location);
-  }
-  // it's client side
-  if (!locale) {
+    const cookieStore = require("next/headers").cookies();
+    locale = cookieStore.get("locale")?.value ?? "en";
+  } else {
     const pathname = window.location.pathname;
     const pathnameParts = pathname.split("/");
     locale = pathnameParts?.[1] ?? "en";
   }
-  return getBaseLinkWithoutLocale(locale + "/" + location);
+  return locale;
 }
-export function getBaseLinkWithoutLocale(location: string) {
-  // if it's server side
-  if (isServerSide()) {
-    return process.env.PROJECT_BASE_URL + "/" + location;
-  }
-  // it's client side
-  return window.location.origin + "/" + location;
+
+export function getBaseLink(location: string,withLocale?:boolean, locale?: string) {
+  const origin = isServerSide() ? process.env.PROJECT_BASE_URL : window.location.origin;
+  const localePath = withLocale ? getLocale(locale) + "/" : "";
+  return `${origin}/${localePath}/${location}`;
 }
