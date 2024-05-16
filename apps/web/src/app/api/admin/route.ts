3a58a316be9af1c7fb49ac6504859d57343b2877
abdurrahman rajab <abdurrahman.rajab@ayasofyazilim.com>
2024-05-16 09:42:42 +0300

    refactor(API): change the api call to config

--- apps/web/src/app/api/admin/route.ts
+++ apps/web/src/app/api/admin/route.ts
@@ -1,10 +1,23 @@
+import { Volo_Abp_Identity_IdentityRoleCreateDto, Volo_Abp_Identity_IdentityRoleUpdateDto } from "@ayasofyazilim/saas/IdentityService";
 import { NextRequest } from "next/server";
 import { getIdentityServiceClient } from "src/lib";
 
-export async function GET(request: NextRequest) {
-  const client = await getIdentityServiceClient(request);
-  const roles = await client.role.getApiIdentityRolesAll();
+const config = {
+  role: {
+    client: async (req: NextRequest) => getIdentityServiceClient(req),
+    get: async (req: NextRequest) => (await config.role.client(req)).role.getApiIdentityRolesAll(),
+    put: (req: NextRequest, id: string, requestBody: Volo_Abp_Identity_IdentityRoleUpdateDto | undefined) => getIdentityServiceClient(req).role.putApiIdentityRolesById({
+      id: id,
+      requestBody: requestBody
+    }),
+    post: (req: NextRequest, requestBody: Volo_Abp_Identity_IdentityRoleCreateDto | undefined) => getIdentityServiceClient(req).role.postApiIdentityRoles({ requestBody }),
+    delete: (req: NextRequest, id: string) => getIdentityServiceClient(req).role.deleteApiIdentityRolesById({ id })
+  }
+}
 
+export async function GET(request: NextRequest) {
+  const roles = await config["role"].get(request);
+  console.log("roles from get ", roles);
   return new Response(JSON.stringify(roles));
 }
 
@@ -18,12 +31,12 @@ export async function POST(request: NextRequest) {
 
 
 export async function DELETE(request: NextRequest) {
-  let retVal =  "something went wrong";
+  let retVal = "something went wrong";
   const client = await getIdentityServiceClient(request);
   const id = await request.json();
   console.log("id from reqest ", id)
   const deleteById = await client.role.deleteApiIdentityRolesById({ id })
-  if(deleteById === undefined) retVal = "successfull"
+  if (deleteById === undefined) retVal = "successfull"
 
   return new Response(JSON.stringify(retVal));
 }
