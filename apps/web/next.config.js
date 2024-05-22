a02f896eecf71e9ad7e70bc856abb3ad8cdca273
Ertuğrulcan Bakas <ecbakas@gmail.com>
2024-05-22 10:34:33 +0300

    chore: next config clean

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,24 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  async rewrites() {
-    return [
-      // `/en/api` yolu için `/api` rotasına yeniden yönlendirme      
-      {
-        source: '/:lang/api/:path*',
-        destination: '/api/:path*',
-      },
-    ];
-  },
   reactStrictMode: true,
-  env: {
-    BASE_URL: process.env.BASE_URL,
-    AUTH_SECRET: process.env.AUTH_SECRET,
-  },
   transpilePackages: ["@repo/ui"],
-  experimental: {
-    serverActions: {
-      allowedOrigins: [ "localhost:3000", "*.devtunnels.ms:3000"  ],
-    }
-  }
+  output: "standalone",
 };
-
