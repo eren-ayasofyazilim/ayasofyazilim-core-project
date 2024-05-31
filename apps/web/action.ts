cdb4f5006bfabc205ed958818263e522b3acf6ff
Eren <ahmeterenodaci@gmail.com>
2024-05-31 17:06:19 +0300

    feat: delete project

--- apps/web/action.ts
+++ apps/web/action.ts
@@ -1,6 +1,9 @@
 "use server";
 import { Volo_Abp_Account_UpdateProfileDto } from "@ayasofyazilim/saas/AccountService";
-import { AbpForDeploy_ProjectService_ProjectsDto_CreateUpdateProjectDto } from "@ayasofyazilim/saas/ProjectService";
+import {
+  AbpForDeploy_ProjectService_ProjectsDto_CreateUpdateProjectDto,
+  DeleteApiProjectServiceProjectsByIdData,
+} from "@ayasofyazilim/saas/ProjectService";
 import { getAccountServiceClient, getProjectServiceClient } from "src/lib";
 
 export async function updateUserProfileServer(
@@ -45,12 +48,33 @@ export async function createUpdateProjectServer(
     };
   }
 }
-export async function getPermission(){
-  const client = await getAccountServiceClient();
-  const response = await client.abpApplicationConfiguration.getApiAbpApplicationConfiguration({
-    includeLocalizationResources: false,
-  });
-  console.log("permission server " , response.auth?.grantedPolicies);
-  return response.auth?.grantedPolicies;
+export async function deleteProjectServer(
+  body: DeleteApiProjectServiceProjectsByIdData
+) {
+  "use server";
+  try {
+    const client = await getProjectServiceClient();
+    const response = await client.project.deleteApiProjectServiceProjectsById({
+      id: body.id,
+    });
 
+    return {
+      status: 200,
+      projectData: response,
+    };
+  } catch (error: any) {
+    console.log(error);
+    return {
+      status: error.status,
+      message: error?.body?.error?.details,
+    };
+  }
+}
+export async function getPermission() {
+  const client = await getAccountServiceClient();
+  const response =
+    await client.abpApplicationConfiguration.getApiAbpApplicationConfiguration({
+      includeLocalizationResources: false,
+    });
+  return response.auth?.grantedPolicies;
 }
