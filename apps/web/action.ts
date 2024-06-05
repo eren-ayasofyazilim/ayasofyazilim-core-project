1e360bfee88b3e2037ca243549e1e38c37977514
Eren <ahmeterenodaci@gmail.com>
2024-06-05 11:20:16 +0300

    refactor: move project related actions

--- apps/web/action.ts
+++ apps/web/action.ts
@@ -1,10 +1,6 @@
 "use server";
 import { Volo_Abp_Account_UpdateProfileDto } from "@ayasofyazilim/saas/AccountService";
-import {
-  AbpForDeploy_ProjectService_ProjectsDto_CreateUpdateProjectDto,
-  DeleteApiProjectServiceProjectsByIdData,
-} from "@ayasofyazilim/saas/ProjectService";
-import { getAccountServiceClient, getProjectServiceClient } from "src/lib";
+import { getAccountServiceClient } from "src/lib";
 
 export async function updateUserProfileServer(
   body: Volo_Abp_Account_UpdateProfileDto
@@ -27,49 +23,7 @@ export async function updateUserProfileServer(
     };
   }
 }
-export async function createUpdateProjectServer(
-  body: AbpForDeploy_ProjectService_ProjectsDto_CreateUpdateProjectDto
-) {
-  "use server";
-  try {
-    const client = await getProjectServiceClient();
-    const response = await client.project.postApiProjectServiceProjects({
-      requestBody: body,
-    });
 
-    return {
-      status: 200,
-      projectData: response,
-    };
-  } catch (error: any) {
-    return {
-      status: error.status,
-      message: error?.body?.error?.details,
-    };
-  }
-}
-export async function deleteProjectServer(
-  body: DeleteApiProjectServiceProjectsByIdData
-) {
-  "use server";
-  try {
-    const client = await getProjectServiceClient();
-    const response = await client.project.deleteApiProjectServiceProjectsById({
-      id: body.id,
-    });
-
-    return {
-      status: 200,
-      projectData: response,
-    };
-  } catch (error: any) {
-    console.log(error);
-    return {
-      status: error.status,
-      message: error?.body?.error?.details,
-    };
-  }
-}
 export async function getPermission() {
   const client = await getAccountServiceClient();
   const response =
