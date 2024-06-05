96321cff19fe6ffb51bea36f6b42dc708e9ab391
Eren <ahmeterenodaci@gmail.com>
2024-06-05 16:28:49 +0300

    refactor: move project related actions

--- apps/web/src/app/[lang]/(main)/projects/[projectId]/details/page.tsx
+++ apps/web/src/app/[lang]/(main)/projects/[projectId]/details/page.tsx
@@ -1,57 +1,24 @@
 "use server";
 
-import {
-  AbpForDeploy_ProjectService_ProjectSections_CreateUpdateProjectSectionDto,
-  Volo_Abp_Application_Dtos_PagedResultDto_1,
-} from "@ayasofyazilim/saas/ProjectService";
+import { Card } from "@/components/ui/card";
 import { redirect } from "next/navigation";
-import { getProjectServiceClient } from "src/lib";
 import { getLocalizationResources } from "src/utils";
+import {
+  getDefaultProjectSectionsServer,
+  getProjectByIdServer,
+  getProjectSectionsServer,
+} from "../../action";
 import ProjectForm from "./form";
 
 export default async function Page({ params }: any) {
   const { projectId } = params;
   const resources = await getLocalizationResources(params.lang);
   if (!resources?.["ProjectService"].texts) return null;
-  async function getData() {
-    "use server";
-    try {
-      const client =
-        await getProjectServiceClient().project.getApiProjectServiceProjectsById(
-          {
-            id: projectId,
-          }
-        );
-      return client;
-    } catch (error) {}
-    return null;
-  }
-  async function getSectionData() {
-    "use server";
-    try {
-      const client =
-        await getProjectServiceClient().projectSection.getApiProjectSectionServiceProjectSection();
-      return client;
-    } catch (error) {
-      return [] as Volo_Abp_Application_Dtos_PagedResultDto_1;
-    }
-  }
-  async function getProjectSectionData() {
-    "use server";
-    try {
-      const client =
-        await getProjectServiceClient().projectSectionRelation.getApiProjectSectionRelationServiceProjectSectionRelation();
 
-      return client.items?.filter(
-        (i) => i.projectId === projectId
-      ) as Volo_Abp_Application_Dtos_PagedResultDto_1["items"];
-    } catch (error) {}
-    return [];
-  }
-  const projectData = await getData();
-  const usedSectionsInProject = ((await getProjectSectionData()) ||
-    []) as Array<AbpForDeploy_ProjectService_ProjectSections_CreateUpdateProjectSectionDto>;
-  const mandatorySections = (await getSectionData()) || [];
+  const projectData = await getProjectByIdServer(projectId);
+  const usedSectionsInProject =
+    (await getProjectSectionsServer(projectId)) || [];
+  const mandatorySections = (await getDefaultProjectSectionsServer()) || [];
 
   mandatorySections?.items?.toReversed()?.forEach((section: any) => {
     const index = usedSectionsInProject.findIndex(
@@ -59,6 +26,7 @@ export default async function Page({ params }: any) {
       (s) => s.projectSectionId === section.id
     );
     if (index > -1 && usedSectionsInProject?.[index]) {
+      /* @ts-ignore */
       usedSectionsInProject[index].name = section.name;
     } else {
       usedSectionsInProject.push(section);
@@ -73,14 +41,14 @@ export default async function Page({ params }: any) {
   if (!projectResource || !uiResource) return;
 
   return (
-    <div className="relative w-full container" id="details">
-      <div className="flex flex-row flex-wrap justify-between mb-8">
+    <div className="relative w-full container mt-8">
+      <Card className="p-6 w-full">
         <ProjectForm
           resources={resources}
           sectionData={usedSectionsInProject}
           projectId={projectId}
         />
-      </div>
+      </Card>
     </div>
   );
 }
