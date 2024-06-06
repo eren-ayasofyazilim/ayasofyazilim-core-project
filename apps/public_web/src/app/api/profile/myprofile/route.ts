ce93f1ccfd0454ba017c6b676e12b2414e27f478
Ahmet Eren Odacı <33299067+ahmeterenodaci@users.noreply.github.com>
2024-06-06 15:38:11 +0300

    Make public web great again (#203)

--- apps/public_web/src/app/api/profile/myprofile/route.ts
+++
@@ -1,8 +0,0 @@
-import { getAccountServiceClient } from "src/lib";
-
-export async function GET(request: Request) {
-  const client = await getAccountServiceClient();
-  const user = await client.profile.getApiAccountMyProfile();
-
-  return new Response(JSON.stringify(user));
-}
