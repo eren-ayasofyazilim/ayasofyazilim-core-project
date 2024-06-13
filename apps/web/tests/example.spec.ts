bd319aace592c3dad224057574b6be965a181f7f
abdurrahman rajab <abdurrahman.rajab@ayasofyazilim.com>
2024-06-13 10:39:13 +0300

    feat: add basic tests

--- apps/web/tests/example.spec.ts
+++ apps/web/tests/example.spec.ts
@@ -1,18 +1,26 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('has title', async ({ page }) => {
-  await page.goto('/');
+require("dotenv").config();
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Unirefund/);
-});
+test.describe("Login page main functionalities", () => {
+  test("has title", async ({ page }) => {
+    await page.goto("/");
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Unirefund/);
+  });
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  test("get started link", async ({ page }) => {
+    await page.goto("/");
+    await page.url();
+    await page.getByPlaceholder("name@example.com").click();
+    await page
+      .getByPlaceholder("name@example.com")
+      .fill(process.env.TEST_USERNAME);
+    await page.getByPlaceholder("name@example.com").press("Tab");
+    await page.getByPlaceholder("Password").fill(process.env.TEST_PASSWORD);
+    await page.getByRole("button", { name: "Login" }).click();
+    await page.url();
+    await page.getByRole("heading", { name: "admin", exact: true }).click();
+  });
 });
