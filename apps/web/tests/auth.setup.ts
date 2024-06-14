2ed69564f9d7e9b766b9b58f58401ea3842fa908
abdurrahman rajab <abdurrahman.rajab@ayasofyazilim.com>
2024-06-14 08:49:46 +0300

    feat: add authentictaion to e2e test

---
+++ apps/web/tests/auth.setup.ts
@@ -0,0 +1,20 @@
+import { test as setup, expect } from '@playwright/test';
+
+const authFile = 'playwright/.auth/user.json';
+
+setup('Authenticate/Login', async ({ page }) => {
+    await page.goto("/");
+    await page.url();
+    await page.getByPlaceholder("name@example.com").click();
+    await page
+      .getByPlaceholder("name@example.com")
+      .fill(process.env.TEST_USERNAME as string);
+    await page.getByPlaceholder("name@example.com").press("Tab");
+    await page
+      .getByPlaceholder("Password")
+      .fill(process.env.TEST_PASSWORD as string);
+    await page.getByRole("button", { name: "Login" }).click();
+    await page.url();
+    await page.getByRole("heading", { name: "admin", exact: true }).click();
+    await page.context().storageState({ path: authFile });
+});
