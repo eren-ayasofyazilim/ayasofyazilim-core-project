b574b5958645677f6fe73f27a8aac45c8f973ff0
abdurrahman rajab <abdurrahman.rajab@ayasofyazilim.com>
2024-06-26 16:37:48 +0300

    feat: add profile page

--- apps/web/src/app/[lang]/app/[type]/profile/page.tsx
+++ apps/web/src/app/[lang]/app/[type]/profile/page.tsx
@@ -1,27 +1,26 @@
 "use client";
 import AutoForm from "@repo/ayasofyazilim-ui/organisms/auto-form";
+import CardList from "@repo/ayasofyazilim-ui/organisms/card-list";
 import { useSession } from "next-auth/react";
-import { use, useEffect } from "react";
-import { getBaseLink } from "src/utils";
+import { use, useEffect, useState } from "react";
+import { createZodObject, getBaseLink } from "src/utils";
 import { z } from "zod";
+import { $UpwithCrowd_BackerService_Organizations_CreateOrganizationDto } from "@ayasofyazilim/saas/BackerService";
+import AutoformDialog from "@repo/ayasofyazilim-ui/molecules/dialog";
+import { Button } from "@/components/ui/button";
 
 export default function Page({
   params,
 }: {
   params: { lang: string; type: string };
 }) {
+  const [open, setOpen] = useState(false);
   const type = params.type;
   const session = useSession();
-
+  const createBacker = $UpwithCrowd_BackerService_Organizations_CreateOrganizationDto;
+  const backerZod = createZodObject(createBacker, ["name", "taxpayerId", "legalStatusCode" ]);
   const formSchema = {
-    admin: z.object({
-      name: z.string(),
-      surname: z.string(),
-      companyName: z.string(),
-      informationNotes: z.string().optional(), // Assuming this field can be optional
-      tc: z.string(), // Assuming TC is a string; adjust the validation as needed
-      phoneNumber: z.string(), // Adjust validation as needed, e.g., regex for phone numbers
-    }),
+    admin: backerZod,
     user: z.object({
       name: z.string(),
       surname: z.string(),
@@ -36,13 +35,7 @@ export default function Page({
       phoneNumber: z.string(),
       email: z.string().email(),
     }),
-    investor: z.object({
-      name: z.string(),
-      surname: z.string(),
-      companyName: z.string(),
-      phoneNumber: z.string(),
-      email: z.string().email(),
-    }),
+    investor: backerZod,
   };
 
   useEffect(() => {
@@ -55,10 +48,56 @@ export default function Page({
   }, [session.data]);
 
   return (
-    <div>
-      <h1>Page</h1>
-      <div className="flex-1">profil + {type}</div>
-      <AutoForm formSchema={formSchema[type]}></AutoForm>
+    <div className="container flex flex-col m-4">
+      <div className="flex-row mb-2">
+        <Button onClick={() => setOpen(true)} className="float-right">New {type}</Button>
+      </div>
+      <AutoformDialog
+        open={open}
+        onOpenChange={setOpen}
+        action={{
+          cta: 'New ' + type,
+          description: 'Add New ' + type,
+          callback: () => {
+            console.log('callback');
+          },
+          autoFormArgs: {
+            formSchema: formSchema[type],
+          },
+        }}
+      />
+      <CardList
+        cards={[
+          {
+            title: 'Profile',
+            content: 'Ahmet Kaya',
+            description: 'Comapny Name',
+            footer: 'Your target is 100%',
+          },
+          {
+            title: 'Profile',
+            content: 'Ahmet Kaya',
+            description: 'Comapny Name',
+            footer: 'Your target is 100%',
+          }, {
+            title: 'Profile',
+            content: 'Ahmet Kaya',
+            description: 'Comapny Name',
+            footer: 'Your target is 100%',
+          }, {
+            title: 'Profile',
+            content: 'Ahmet Kaya',
+            description: 'Comapny Name',
+            footer: 'Your target is 100%',
+          },
+          {
+            title: 'Profile',
+            content: 'Ahmet Kaya',
+            description: 'Comapny Name',
+            footer: 'Your target is 100%',
+          },
+        ]}
+      ></CardList>
     </div>
   );
 }
