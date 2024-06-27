647af9a38457a87faf8d1225ccadf8552cee43c5
yusualhashash <yusufalhashash2018@gmail.com>
2024-06-27 16:23:02 +0300

    feat: add fetchUsers function to show users into organization page

--- apps/web/src/app/[lang]/(main)/dashboard/[domain]/organization/action.ts
+++ apps/web/src/app/[lang]/(main)/dashboard/[domain]/organization/action.ts
@@ -6,6 +6,12 @@ export interface OrganizationUnit {
   id: string;
 }
 
+export interface User {
+  id: string;
+  userName: string;
+  email: string;
+}
+
 export const fetchOrganizationUnits = async (): Promise<OrganizationUnit[]> => {
   try {
     const response = await fetch(getBaseLink("api/admin/organization"));
@@ -18,6 +24,22 @@ export const fetchOrganizationUnits = async (): Promise<OrganizationUnit[]> => {
     }
   } catch (error) {
     console.error("Error fetching organization units:", error);
+    return [];
+  }
+};
+
+export const fetchUsers = async (): Promise<User[]> => {
+  try {
+    const response = await fetch(getBaseLink("api/admin/user"));
+    if (response.ok) {
+      const data = await response.json();
+      return data.items;
+    } else {
+      console.error("Failed to fetch users", response.statusText);
+      return [];
+    }
+  } catch (error) {
+    console.error("Error fetching users:", error);
     return [];
   }
 };
