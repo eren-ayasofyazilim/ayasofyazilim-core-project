901cee914ac98a9a25237c1893975ff39349b29d
Eren <ahmeterenodaci@gmail.com>
2024-07-01 09:01:34 +0300

    feat: add project preview

---
+++ apps/web/src/app/[lang]/app/[type]/projects/[projectId]/preview/page.tsx
@@ -0,0 +1,49 @@
+"use server";
+
+import ScrollArea from "@repo/ayasofyazilim-ui/molecules/scroll-area";
+import TipTapEditor from "@repo/ayasofyazilim-ui/organisms/tiptap";
+import ProjectPage from "@repo/ui/upwithcrowd/project/project-page";
+import { ProjectStatusEnums } from "src/enums/project";
+import { getResourceData } from "src/language-data/Projects/projects";
+import { getProjectServiceClient } from "src/lib";
+
+export default async function Page({
+  params,
+}: {
+  params: { projectId: string; lang: string };
+}) {
+  const { projectId, lang } = params;
+  const { languageData, resources } = await getResourceData(lang);
+
+  const { project: projectData, projectSectionRelations: projectSectionData } =
+    await getProjectServiceClient().project.getApiProjectServiceProjectsDetailById(
+      { id: projectId }
+    );
+
+  if (!projectData) return null;
+
+  const sectionsData =
+    projectSectionData?.map((section: any, index) => ({
+      key: section.sectionName || "" + index,
+      id: section.sectionName.replaceAll(" ", ""),
+      name: section.sectionName,
+      value: (
+        <TipTapEditor
+          editorContent={JSON.parse(section.sectionRelationValue || "{}")}
+          canEditable={false}
+        />
+      ),
+    })) || [];
+
+  return (
+    <ScrollArea className="z-10">
+      <ProjectPage
+        projectData={projectData}
+        sectionsData={sectionsData}
+        languageData={languageData}
+        ProjectStatusEnums={ProjectStatusEnums}
+        isPreview={true}
+      />
+    </ScrollArea>
+  );
+}
