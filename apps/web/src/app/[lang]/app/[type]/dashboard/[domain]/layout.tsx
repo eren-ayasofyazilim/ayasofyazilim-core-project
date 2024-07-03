76cb3c2e122f09b083deb374cbf74cdd2ba47bb9
Eren <ahmeterenodaci@gmail.com>
2024-07-03 16:42:34 +0300

    refactor: dashboard [domain

--- apps/web/src/app/[lang]/app/[type]/dashboard/[domain]/layout.tsx
+++ apps/web/src/app/[lang]/app/[type]/dashboard/[domain]/layout.tsx
@@ -5,6 +5,8 @@ import { usePathname, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { dataConfig } from "../data";
 import { getBaseLink } from "src/utils";
+import { useLocale } from "src/providers/locale";
+import { getResourceDataClient } from "src/language-data/AbpUiNavigation/navbar";
 
 function isPathValid(path: string, navbarItems: any[]) {
   const validPaths = navbarItems.map((item) => item.id);
@@ -17,25 +19,32 @@ type LayoutProps = {
 };
 
 export default function Layout({ children, params }: LayoutProps) {
-  const [navbarItems, setnavbarItems] = useState<any>([]);
-  const router = useRouter();
+  const [navbarItems, setNavbarItems] = useState<any>([]);
+  const { resources } = useLocale();
+
+  const languageData = getResourceDataClient(resources);
   const pathname = usePathname();
   const path = pathname.split("dashboard/")?.[1];
-
+  console.log(languageData);
   useEffect(() => {
-    if (!path || isPathValid(path, navbarItems) === false) {
-      //   router.push("dashboardfsdfasdf/role");
-    }
     const tempNavbarItems = Object.entries(dataConfig[params.domain])
       .filter(([e, value]) => e !== "displayName" && e !== "default")
       .map(([key, value]: [any, any]) => ({
         id: `${params.domain}/${key}`,
-        name: key,
-        link: getBaseLink(`dashboard/${params.domain}/${key}`, true),
+        name:
+          languageData?.[("Identity:" + key) as keyof typeof languageData] ||
+          key,
+        link: getBaseLink(
+          `dashboard/${params.domain}/${key}`,
+          true,
+          params.lang,
+          true,
+          params.type
+        ),
       }));
-    setnavbarItems(tempNavbarItems);
+    setNavbarItems(tempNavbarItems);
   }, []);
-  console.log("path", path);
+
   return (
     <>
       {navbarItems.length > 0 && (
