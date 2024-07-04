4b3d1cb1947e7bc331b469d6d8a503a28f895b41
eren-ayasofyazilim <163110882+eren-ayasofyazilim@users.noreply.github.com>
2024-07-04 15:02:50 +0300

    refactor: clean public_web (#313)

--- apps/public_web/src/app/[lang]/(main)/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-// import Dashboard from '@repo/ayasofyazilim-ui/templates/dashboard';
-
-export default function Page(): JSX.Element {
-  return (
-    <></>
-    // <Dashboard />
-  );
-}
