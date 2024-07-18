6cdd27ff4cfd13dbd83d4104c2155032c5b9a66a
abdurrahman rajab <abdurrahman.rajab@ayasofyazilim.com>
2024-07-18 15:09:52 +0300

    test: add basi navigation to project

--- apps/web/tests/upwithcrowd/main-page.spec.ts
+++ apps/web/tests/upwithcrowd/main-page.spec.ts
@@ -24,30 +24,4 @@ test.describe("Main Page testing", () => {
       index++;
     }
   });
-  test("Navigating the website", async ({ page }) => {
-    // await loginAsAdmin(page);
-    await page.goto("/en/public");
-    await page
-      .locator("div")
-      .filter({ hasText: /^istanbulGo to city page$/ })
-      .getByRole("button")
-      .click();
-    await page.getByRole("menuitem", { name: "Campaigns" }).click();
-    await expect(page.getByText("No Projects")).toBeVisible();
-    await page.getByRole("button", { name: "admin admin@abp.io" }).click();
-    await page.getByRole("link", { name: "Admin Center" }).click();
-    await page.goto("/en/app/admin");
-    await expect(
-      page.getByRole("link", { name: "Identity Management" }),
-    ).toBeVisible();
-    await page.getByRole("link", { name: "Profile" }).click();
-    await expect(page.getByRole("link", { name: "Profile" })).toBeVisible();
-    await expect(page.getByRole("link", { name: "Campaigns" })).toBeVisible();
-    await expect(
-      page.getByRole("link", { name: "Language Management" }),
-    ).toBeVisible();
-    await expect(page.getByRole("link", { name: "New admin" })).toBeVisible();
-    await page.getByRole("link", { name: "New admin" }).click();
-    await page.goto("/en/app/admin/profile/new");
-  });
 });
