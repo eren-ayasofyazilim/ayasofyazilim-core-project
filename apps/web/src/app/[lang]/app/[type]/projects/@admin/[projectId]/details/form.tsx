cf645f7eddcfc7998c2467e43e88c855ba8e5af3
Eren <ahmeterenodaci@gmail.com>
2024-07-31 15:08:28 +0300

    fix: section detail bug

--- apps/web/src/app/[lang]/app/[type]/projects/@admin/[projectId]/details/form.tsx
+++ apps/web/src/app/[lang]/app/[type]/projects/@admin/[projectId]/details/form.tsx
@@ -4,8 +4,6 @@ import {
   SectionLayout,
   SectionLayoutContent,
 } from "@repo/ayasofyazilim-ui/templates/section-layout-v2";
-import { useState } from "react";
-import { createOrUpdateProjectSectionRelationServer } from "../../../action";
 
 export interface IProjectFormProps {
   projectId: string;
@@ -21,44 +19,16 @@ export interface IProjectFormProps {
     | null;
   canSectionsEditable: boolean;
 }
-export default function ProjectForm({
-  sectionData,
-  canSectionsEditable,
-}: IProjectFormProps) {
-  const [formValues, setFormValues] = useState<Record<string, number>>(() => {
-    const data: Record<string, number> = {};
-    sectionData?.forEach((section) => {
-      if (section.sectionId) {
-        data[section.sectionId] = section.sectionRelationValue?.length || 0;
-      }
-    });
-    return data;
-  });
+export default function ProjectForm({ sectionData }: IProjectFormProps) {
   if (!sectionData) return <></>;
-
-  async function onSaveClick(editorId: string, editorContent: string) {
-    const section = sectionData?.find((i) => i.sectionId === editorId);
-    if (!section) return "ERROR";
-
-    try {
-      await createOrUpdateProjectSectionRelationServer(section, editorContent);
-      return "OK";
-    } catch (error) {
-      return "ERROR";
-    }
-  }
-  function onWordCountChanged(id: string, count: number) {
-    if (formValues[id] === count) return;
-
-    setFormValues({ ...formValues, [id]: count });
-  }
-
-  const sections = sectionData.map((section, index) => {
-    return {
-      id: section.sectionId || `item${index}`,
-      name: section.sectionName || `item${index}`,
-    };
-  });
+  const sections = sectionData
+    .filter((section) => (section.sectionRelationValue?.length || 0) > 10)
+    .map((section, index) => {
+      return {
+        id: section.sectionId || `item${index}`,
+        name: section.sectionName || `item${index}`,
+      };
+    });
 
   return (
     <SectionLayout
@@ -74,18 +44,15 @@ export default function ProjectForm({
           sectionId={section.sectionId || `item${index}`}
         >
           <TipTapEditor
-            canEditable={canSectionsEditable}
-            editOnStart
+            key={section.sectionId + "-editor" || `item${index}-editor`}
+            canEditable={false}
+            editOnStart={false}
             editorContent={
               section.sectionRelationValue
                 ? JSON.parse(section.sectionRelationValue)
                 : undefined
             }
             editorId={section.sectionId}
-            onSaveFunction={onSaveClick}
-            onWordCountChanged={(v) => {
-              onWordCountChanged(section.sectionId || "", v);
-            }}
           />
         </SectionLayoutContent>
       ))}
--- apps/web/src/app/[lang]/app/[type]/projects/@entrepreneur/new/form.tsx
+++ apps/web/src/app/[lang]/app/[type]/projects/@entrepreneur/new/form.tsx
@@ -290,7 +290,7 @@ export default function NewProjectForm({
                 <Link
                   href={getBaseLink(
                     `app/entrepreneur/projects/${projectId}`,
-                    true
+                    true,
                   )}
                 >
                   {languageData.ViewProject}
