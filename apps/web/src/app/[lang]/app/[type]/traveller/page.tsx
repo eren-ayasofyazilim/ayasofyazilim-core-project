147d42e8b822cef92ab3dd57c06addb9b816511f
Eren <ahmeterenodaci@gmail.com>
2024-08-26 14:12:17 +0300

    chore: add traveller page

---
+++ apps/web/src/app/[lang]/app/[type]/traveller/page.tsx
@@ -0,0 +1,9 @@
+// "use server";
+import React from "react";
+// import { getResourceData } from "src/language-data/ContractService";
+import Table from "./table";
+
+export default function Page() {
+  // const { languageData } = await getResourceData(params.lang);
+  return <Table />;
+}
