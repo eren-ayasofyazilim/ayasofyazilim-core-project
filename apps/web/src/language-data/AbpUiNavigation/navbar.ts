742ae503413121604151649efadc01903c2d8a66
Eren <ahmeterenodaci@gmail.com>
2024-08-26 11:29:00 +0300

    fix: language error when there is no translation

--- apps/web/src/language-data/AbpUiNavigation/navbar.ts
+++ apps/web/src/language-data/AbpUiNavigation/navbar.ts
@@ -14,66 +14,136 @@ function getLanguageData(
 ): Record<string, string> {
   const navigationResource = resources.AbpUiNavigation?.texts;
   return {
-    HomePage: navigationResource?.HomePage || data[lang].HomePage,
-    Dashboard: navigationResource?.Dashboard || data[lang].Dashboard,
-    Management: navigationResource?.Management || data[lang].Management,
-    Crm: navigationResource?.Crm || data[lang].Crm,
-    Profile: navigationResource?.Profile || data[lang].Profile,
-    Details: navigationResource?.Details || data[lang].Details,
-    Companies: navigationResource?.Companies || data[lang].Companies,
-    Template: navigationResource?.Template || data[lang].Template,
+    HomePage:
+      navigationResource?.HomePage || data[lang].HomePage || data.en.Homepage,
+    Dashboard:
+      navigationResource?.Dashboard ||
+      data[lang].Dashboard ||
+      data.en.Dashboard,
+    Management:
+      navigationResource?.Management ||
+      data[lang].Management ||
+      data.en.Management,
+    Crm: navigationResource?.Crm || data[lang].Crm || data.en.Crm,
+    Profile:
+      navigationResource?.Profile || data[lang].Profile || data.en.Profile,
+    Details:
+      navigationResource?.Details || data[lang].Details || data.en.Details,
+    Companies:
+      navigationResource?.Companies ||
+      data[lang].Companies ||
+      data.en.Companies,
+    Template:
+      navigationResource?.Template || data[lang].Template || data.en.Template,
     CountrySettings:
-      navigationResource?.CountrySettings || data[lang].CountrySettings,
-    Contracts: navigationResource?.Contracts || data[lang].Contracts,
-    RebateTables: navigationResource?.RebateTables || data[lang].RebateTables,
-    RefundTables: navigationResource?.RefundTables || data[lang].RefundTables,
-    Settings: navigationResource?.Settings || data[lang].Settings,
+      navigationResource?.CountrySettings ||
+      data[lang].CountrySettings ||
+      data.en.CountrySettings,
+    Contracts:
+      navigationResource?.Contracts ||
+      data[lang].Contracts ||
+      data.en.Contracts,
+    RebateTables:
+      navigationResource?.RebateTables ||
+      data[lang].RebateTables ||
+      data.en.RebateTables,
+    RefundTables:
+      navigationResource?.RefundTables ||
+      data[lang].RefundTables ||
+      data.en.RefundTables,
+    Settings:
+      navigationResource?.Settings || data[lang].Settings || data.en.Settings,
     LanguageManagement:
-      navigationResource?.LanguageManagement || data[lang].LanguageManagement,
+      navigationResource?.LanguageManagement ||
+      data[lang].LanguageManagement ||
+      data.en.LanguageManagement,
     IdentityManagement:
-      navigationResource?.IdentityManagement || data[lang].IdentityManagement,
+      navigationResource?.IdentityManagement ||
+      data[lang].IdentityManagement ||
+      data.en.IdentityManagement,
     SaasManagement:
-      navigationResource?.SaasManagement || data[lang].SaasManagement,
+      navigationResource?.SaasManagement ||
+      data[lang].SaasManagement ||
+      data.en.SaasManagement,
     "Identity:role":
-      navigationResource?.["Identity:role"] || data[lang]["Identity:role"],
+      navigationResource?.["Identity:role"] ||
+      data[lang]["Identity:role"] ||
+      data.en["Identity:role"],
     "Identity:user":
-      navigationResource?.["Identity:user"] || data[lang]["Identity:user"],
+      navigationResource?.["Identity:user"] ||
+      data[lang]["Identity:user"] ||
+      data.en["Identity:user"],
     "Identity:claimType":
       navigationResource?.["Identity:claimType"] ||
-      data[lang]["Identity:claimType"],
+      data[lang]["Identity:claimType"] ||
+      data.en["Identity:claimType"],
 
-    Investor: navigationResource?.Investor || data[lang].Investor,
-    Invest: navigationResource?.Invest || data[lang].Invest,
+    Investor:
+      navigationResource?.Investor || data[lang].Investor || data.en.Investor,
+    Invest: navigationResource?.Invest || data[lang].Invest || data.en.Invest,
     SupportCenter:
-      navigationResource?.SupportCenter || data[lang].SupportCenter,
-    Entrepreneur: navigationResource?.Entrepreneur || data[lang].Entrepreneur,
+      navigationResource?.SupportCenter ||
+      data[lang].SupportCenter ||
+      data.en.SupportCenter,
+    Entrepreneur:
+      navigationResource?.Entrepreneur ||
+      data[lang].Entrepreneur ||
+      data.en.Entrepreneur,
     SubmitYourProject:
-      navigationResource?.SubmitYourProject || data[lang].SubmitYourProject,
+      navigationResource?.SubmitYourProject ||
+      data[lang].SubmitYourProject ||
+      data.en.SubmitYourProject,
     HowDoIFindTheNecessaryFunds:
       navigationResource?.HowDoIFindTheNecessaryFunds ||
-      data[lang].HowDoIFindTheNecessaryFunds,
+      data[lang].HowDoIFindTheNecessaryFunds ||
+      data.en.HowDoIFindTheNecessaryFunds,
     Institutional:
-      navigationResource?.Institutional || data[lang].Institutional,
-    AboutUs: navigationResource?.AboutUs || data[lang].AboutUs,
-    Contact: navigationResource?.Contact || data[lang].Contact,
-    OurTeam: navigationResource?.OurTeam || data[lang].OurTeam,
+      navigationResource?.Institutional ||
+      data[lang].Institutional ||
+      data.en.Institutional,
+    AboutUs:
+      navigationResource?.AboutUs || data[lang].AboutUs || data.en.AboutUs,
+    Contact:
+      navigationResource?.Contact || data[lang].Contact || data.en.Contact,
+    OurTeam:
+      navigationResource?.OurTeam || data[lang].OurTeam || data.en.OurTeam,
     BoardOfDirectors:
-      navigationResource?.BoardOfDirectors || data[lang].BoardOfDirectors,
+      navigationResource?.BoardOfDirectors ||
+      data[lang].BoardOfDirectors ||
+      data.en.BoardOfDirectors,
     InvestingCommittee:
-      navigationResource?.InvestingCommittee || data[lang].InvestingCommittee,
-    Campaigns: navigationResource?.Campaigns || data[lang].Campaigns,
-    AdminCenter: navigationResource?.AdminCenter || data[lang].AdminCenter,
+      navigationResource?.InvestingCommittee ||
+      data[lang].InvestingCommittee ||
+      data.en.InvestingCommittee,
+    Campaigns:
+      navigationResource?.Campaigns ||
+      data[lang].Campaigns ||
+      data.en.Campaigns,
+    AdminCenter:
+      navigationResource?.AdminCenter ||
+      data[lang].AdminCenter ||
+      data.en.AdminCenter,
     EntrepreneurCenter:
-      navigationResource?.EntrepreneurCenter || data[lang].EntrepreneurCenter,
+      navigationResource?.EntrepreneurCenter ||
+      data[lang].EntrepreneurCenter ||
+      data.en.EntrepreneurCenter,
     InvestorCenter:
-      navigationResource?.InvestorCenter || data[lang].InvestorCenter,
+      navigationResource?.InvestorCenter ||
+      data[lang].InvestorCenter ||
+      data.en.InvestorCenter,
     ChangeProfile:
-      navigationResource?.ChangeProfile || data[lang].ChangeProfile,
-    LogIn: navigationResource?.LogIn || data[lang].LogIn,
-    Register: navigationResource?.Register || data[lang].Register,
-    LogOut: navigationResource?.LogOut || data[lang].LogOut,
-    MyAccount: navigationResource?.MyAccount || data[lang].MyAccount,
-    Add: navigationResource?.Add || data[lang].Add,
+      navigationResource?.ChangeProfile ||
+      data[lang].ChangeProfile ||
+      data.en.ChangeProfile,
+    LogIn: navigationResource?.LogIn || data[lang].LogIn || data.en.LogIn,
+    Register:
+      navigationResource?.Register || data[lang].Register || data.en.Register,
+    LogOut: navigationResource?.LogOut || data[lang].LogOut || data.en.LogOut,
+    MyAccount:
+      navigationResource?.MyAccount ||
+      data[lang].MyAccount ||
+      data.en.MyAccount,
+    Add: navigationResource?.Add || data[lang].Add || data.en.Add,
   };
 }
 export async function getResourceData(lang: string): Promise<{
--- apps/web/src/language-data/ContractService/index.ts
+++ apps/web/src/language-data/ContractService/index.ts
@@ -13,83 +13,119 @@ function getLanguageData(
   lang: string,
 ): Record<string, string> {
   const resource = resources.ContractService?.texts;
+  const navigationResource = resources.Navigation?.texts;
   return {
-    HomePage: resource?.HomePage || data[lang].HomePage,
-    EditTemplate: resource?.EditTemplate || data[lang].EditTemplate,
-    RebateSet: resource?.RebateSet || data[lang].RebateSet,
-    ProcessingFees: resource?.ProcessingFees || data[lang].ProcessingFees,
-    CancelButton: resource?.CancelButton || data[lang].CancelButton,
-    SaveButton: resource?.SaveButton || data[lang].SaveButton,
-    RefundMethod: resource?.RefundMethod || data[lang].RefundMethod,
-    FixedFee: resource?.FixedFee || data[lang].FixedFee,
-    VariableFee: resource?.VariableFee || data[lang].VariableFee,
-    Percent: resource?.Percent || data[lang].Percent,
-    Name: resource?.Name || data[lang].Name,
-    Amount: resource?.Amount || data[lang].Amount,
-    All: resource?.All || data[lang].All,
-    Cash: resource?.Cash || data[lang].Cash,
+    HomePage:
+      navigationResource?.HomePage || data[lang].HomePage || data.en.HomePage,
+    EditTemplate:
+      resource?.EditTemplate || data[lang].EditTemplate || data.en.EditTemplate,
+    RebateSet: resource?.RebateSet || data[lang].RebateSet || data.en.RebateSet,
+    ProcessingFees:
+      resource?.ProcessingFees ||
+      data[lang].ProcessingFees ||
+      data.en.ProcessingFees,
+    CancelButton:
+      resource?.CancelButton || data[lang].CancelButton || data.en.CancelButton,
+    SaveButton:
+      resource?.SaveButton || data[lang].SaveButton || data.en.SaveButton,
+    RefundMethod:
+      resource?.RefundMethod || data[lang].RefundMethod || data.en.RefundMethod,
+    FixedFee: resource?.FixedFee || data[lang].FixedFee || data.en.FixedFee,
+    VariableFee:
+      resource?.VariableFee || data[lang].VariableFee || data.en.VariableFee,
+    Percent: resource?.Percent || data[lang].Percent || data.en.Percent,
+    Name: resource?.Name || data[lang].Name || data.en.Name,
+    Amount: resource?.Amount || data[lang].Amount || data.en.Amount,
+    All: resource?.All || data[lang].All || data.en.All,
+    Cash: resource?.Cash || data[lang].Cash || data.en.Cash,
     CreditOrDebitCard:
-      resource?.CreditOrDebitCard || data[lang].CreditOrDebitCard,
-    Alipay: resource?.Alipay || data[lang].Alipay,
-    WeChat: resource?.WeChat || data[lang].WeChat,
-    CashViaPartner: resource?.CashViaPartner || data[lang].CashViaPartner,
-    RefundLater: resource?.RefundLater || data[lang].RefundLater,
-    Refund: resource?.Refund || data[lang].Refund,
-    RefundTables: resource?.RefundTables || data[lang].RefundTables,
+      resource?.CreditOrDebitCard ||
+      data[lang].CreditOrDebitCard ||
+      data.en.CreditOrDebitCard,
+    Alipay: resource?.Alipay || data[lang].Alipay || data.en.Alipay,
+    WeChat: resource?.WeChat || data[lang].WeChat || data.en.WeChat,
+    CashViaPartner:
+      resource?.CashViaPartner ||
+      data[lang].CashViaPartner ||
+      data.en.CashViaPartner,
+    RefundLater:
+      resource?.RefundLater || data[lang].RefundLater || data.en.RefundLater,
+    Refund: resource?.Refund || data[lang].Refund || data.en.Refund,
+    RefundTables:
+      resource?.RefundTables || data[lang].RefundTables || data.en.RefundTables,
     "RefundTables.Details":
-      resource?.["RefundTables.Details"] || data[lang]["RefundTables.Details"],
+      resource?.["RefundTables.Details"] ||
+      data[lang]["RefundTables.Details"] ||
+      data.en["RefundTables.Details"],
     "RefundTables.Create.Title":
       resource?.["RefundTables.Create.Title"] ||
-      data[lang]["RefundTables.Create.Title"],
+      data[lang]["RefundTables.Create.Title"] ||
+      data.en["RefundTables.Create.Title"],
     "RefundTables.Create.Description":
       resource?.["RefundTables.Create.Title"] ||
-      data[lang]["RefundTables.Create.Description"],
+      data[lang]["RefundTables.Create.Description"] ||
+      data.en["RefundTables.Create.Description"],
     "RefundTables.Edit.Title":
       resource?.["RefundTables.Create.Title"] ||
-      data[lang]["RefundTables.Edit.Title"],
+      data[lang]["RefundTables.Edit.Title"] ||
+      data.en["RefundTables.Edit.Title"],
     "RefundTables.Edit.Description":
       resource?.["RefundTables.Create.Title"] ||
-      data[lang]["RefundTables.Edit.Description"],
+      data[lang]["RefundTables.Edit.Description"] ||
+      data.en["RefundTables.Edit.Description"],
     "RefundTables.Delete.Title":
       resource?.["RefundTables.Create.Title"] ||
-      data[lang]["RefundTables.Delete.Title"],
+      data[lang]["RefundTables.Delete.Title"] ||
+      data.en["RefundTables.Delete.Title"],
     "RefundTables.Delete.Description":
       resource?.["RefundTables.Create.Title"] ||
-      data[lang]["RefundTables.Delete.Description"],
+      data[lang]["RefundTables.Delete.Description"] ||
+      data.en["RefundTables.Delete.Description"],
 
     "RefundTables.Name":
       resource?.["RefundTables.Create.Title"] ||
-      data[lang]["RefundTables.Name"],
+      data[lang]["RefundTables.Name"] ||
+      data.en["RefundTables.Name"],
     "RefundTables.ValidFrom":
       resource?.["RefundTables.Create.Title"] ||
-      data[lang]["RefundTables.ValidFrom"],
+      data[lang]["RefundTables.ValidFrom"] ||
+      data.en["RefundTables.ValidFrom"],
     "RefundTables.ValidTo":
       resource?.["RefundTables.Create.Title"] ||
-      data[lang]["RefundTables.ValidTo"],
+      data[lang]["RefundTables.ValidTo"] ||
+      data.en["RefundTables.ValidTo"],
     "RefundTables.IsDefault":
       resource?.["RefundTables.Create.Title"] ||
-      data[lang]["RefundTables.IsDefault"],
+      data[lang]["RefundTables.IsDefault"] ||
+      data.en["RefundTables.IsDefault"],
     "RefundTables.IsBundling":
       resource?.["RefundTables.Create.Title"] ||
-      data[lang]["RefundTables.IsBundling"],
+      data[lang]["RefundTables.IsBundling"] ||
+      data.en["RefundTables.IsBundling"],
     "RefundTables.LastModificationTime":
       resource?.["RefundTables.Create.Title"] ||
-      data[lang]["RefundTables.LastModificationTime"],
+      data[lang]["RefundTables.LastModificationTime"] ||
+      data.en["RefundTables.LastModificationTime"],
     "RefundTables.Actions":
       resource?.["RefundTables.Create.Title"] ||
-      data[lang]["RefundTables.Actions"],
+      data[lang]["RefundTables.Actions"] ||
+      data.en["RefundTables.Actions"],
     "RefundTables.Edit":
       resource?.["RefundTables.Create.Title"] ||
-      data[lang]["RefundTables.Edit"],
+      data[lang]["RefundTables.Edit"] ||
+      data.en["RefundTables.Edit"],
     "RefundTables.Delete":
       resource?.["RefundTables.Create.Title"] ||
-      data[lang]["RefundTables.Delete"],
+      data[lang]["RefundTables.Delete"] ||
+      data.en["RefundTables.Delete"],
     "RefundTables.Cancel":
       resource?.["RefundTables.Create.Title"] ||
-      data[lang]["RefundTables.Cancel"],
+      data[lang]["RefundTables.Cancel"] ||
+      data.en["RefundTables.Cancel"],
     "RefundTables.Save":
       resource?.["RefundTables.Create.Title"] ||
-      data[lang]["RefundTables.Save"],
+      data[lang]["RefundTables.Save"] ||
+      data.en["RefundTables.Save"],
   };
 }
 export async function getResourceData(lang: string): Promise<{
