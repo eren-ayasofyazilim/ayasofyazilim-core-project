5497669c2b9fe0f22fdbd69eb5ba1840436814a2
Eren <ahmeterenodaci@gmail.com>
2024-08-26 09:45:17 +0300

    refactor: move language selector to profile menu on admin panel

--- packages/ui/src/utils.ts
+++ packages/ui/src/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+export type ResourceResult = Record<
+  string,
+  | {
+      texts?: Record<string, string> | null | undefined;
+      baseResources?: string[] | null | undefined;
+    }
+  | undefined
+>;
