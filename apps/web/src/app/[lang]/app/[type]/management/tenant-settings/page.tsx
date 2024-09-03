66031b48b2949e0f2e0b16fcdbd799384ce63dca
Eren <ahmeterenodaci@gmail.com>
2024-09-03 15:06:11 +0300

    fix: tenant settings default redirect

--- apps/web/src/app/[lang]/app/[type]/management/tenant-settings/page.tsx
+++ apps/web/src/app/[lang]/app/[type]/management/tenant-settings/page.tsx
@@ -1,6 +1,17 @@
 import { RedirectType, permanentRedirect } from "next/navigation";
 import { getBaseLink } from "src/utils";
 
-export default function Page() {
-  permanentRedirect(getBaseLink("country-settings/home"), RedirectType.push);
+export default function Page({
+  params,
+}: {
+  params: { lang: string; type: string };
+}) {
+  permanentRedirect(
+    getBaseLink(
+      `app/${params.type}/management/tenant-settings/home`,
+      true,
+      params.lang,
+    ),
+    RedirectType.push,
+  );
 }
