368e7ab8412e310c476b5606f7e1332b7a677ae8
Abdurrahman Rajab <rajab-2@windowslive.com>
2024-09-04 01:09:40 +0300

    chore: remove extra code

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -143,10 +143,4 @@ export const middleware = auth((request: NextAuthRequest) => {
 
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
-  unstable_allowDynamic: [
-    // allows a single file
-    "/lib/utilities.js",
-    // use a glob to allow anything in the function-bind 3rd party module
-    "node_modules/**",
-  ],
 };
