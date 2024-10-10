83247f980612ced12778d2b2012b9fdc6df8c9ad
yusualhashash <yusufalhashash2018@gmail.com>
2024-10-10 16:44:29 +0300

    refactor:  edit the names of translation text

--- apps/web/src/app/[lang]/app/[type]/crm/traveller/new/form.tsx
+++ apps/web/src/app/[lang]/app/[type]/crm/traveller/new/form.tsx
@@ -139,15 +139,16 @@ export default function Form({
     return data.find((item) => item.name === value)?.code2 || "";
   }
 
-  const translationForm = createFieldConfigWithResource({
+  const translatedForm = createFieldConfigWithResource({
     schema: createTravellerSchema,
     resources: languageData,
+    name: "",
   });
 
   return (
     <AutoForm
       className="grid grid-flow-row  gap-4 space-y-0 lg:grid-cols-3 lg:grid-rows-1"
-      fieldConfig={mergeFieldConfigs(translationForm, {
+      fieldConfig={mergeFieldConfigs(translatedForm, {
         telephone: {
           localNumber: {
             fieldType: "phone",
