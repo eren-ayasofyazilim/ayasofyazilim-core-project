b90ffffe6995f886d763f60b51c4caf64e7f7afd
yusualhashash <yusufalhashash2018@gmail.com>
2024-10-10 16:45:45 +0300

    fix: remove name from translation function to work

--- apps/web/src/app/[lang]/app/[type]/crm/traveller/new/form.tsx
+++ apps/web/src/app/[lang]/app/[type]/crm/traveller/new/form.tsx
@@ -142,7 +142,6 @@ export default function Form({
   const translatedForm = createFieldConfigWithResource({
     schema: createTravellerSchema,
     resources: languageData,
-    name: "",
   });
 
   return (
