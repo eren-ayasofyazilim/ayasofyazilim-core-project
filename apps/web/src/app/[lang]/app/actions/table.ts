50eed8d5ecfedab8d3850f09ec84306af5dda59d
Eren <ahmeterenodaci@gmail.com>
2024-10-15 10:45:42 +0300

    feat: add global table actions

---
+++ apps/web/src/app/[lang]/app/actions/table.ts
@@ -0,0 +1,133 @@
+"use server";
+
+import { getCRMServiceClient, structuredError } from "src/lib";
+import type {
+  GetCustomsDTO,
+  GetMerchantDTO,
+  GetRefundPointDTO,
+  GetTaxFreeDTO,
+  GetTaxOfficeDTO,
+} from "../[type]/parties/types";
+
+export type TableDataTypes = keyof Awaited<
+  ReturnType<typeof tableDataRequests>
+>;
+async function tableDataRequests() {
+  const client = await getCRMServiceClient();
+  const tableRequests = {
+    merchants: {
+      getDetail: async (id: string) =>
+        (await client.merchant.getApiCrmServiceMerchantsByIdDetail({ id }))
+          .merchant,
+      get: async (data: { maxResultCount: number; skipCount: number }) =>
+        (await client.merchant.getApiCrmServiceMerchants(
+          data,
+        )) as GetMerchantDTO,
+
+      deleteRow: async (id: string) =>
+        await client.merchant.deleteApiCrmServiceMerchantsByIdWithComponents({
+          id,
+        }),
+    } as const,
+    "refund-points": {
+      getDetail: async (id: string) =>
+        await client.refundPoint.getApiCrmServiceRefundPointsByIdDetail({ id }),
+      get: async (data: { maxResultCount: number; skipCount: number }) =>
+        (await client.refundPoint.getApiCrmServiceRefundPoints(
+          data,
+        )) as GetRefundPointDTO,
+
+      deleteRow: async (id: string) =>
+        await client.refundPoint.deleteApiCrmServiceRefundPointsByIdWithComponents(
+          {
+            id,
+          },
+        ),
+    },
+    customs: {
+      getDetail: async (id: string) =>
+        await client.customs.getApiCrmServiceCustomsByIdDetail({ id }),
+      get: async (data: { maxResultCount: number; skipCount: number }) =>
+        (await client.customs.getApiCrmServiceCustoms(data)) as GetCustomsDTO,
+      deleteRow: async (id: string) =>
+        await client.customs.deleteApiCrmServiceCustomsByIdWithComponents({
+          id,
+        }),
+    },
+    "tax-free": {
+      getDetail: async (id: string) =>
+        await client.taxFree.getApiCrmServiceTaxFreesByIdDetail({ id }),
+      get: async (data: { maxResultCount: number; skipCount: number }) =>
+        (await client.taxFree.getApiCrmServiceTaxFrees(data)) as GetTaxFreeDTO,
+
+      deleteRow: async (id: string) =>
+        await client.taxFree.deleteApiCrmServiceTaxFreesByIdWithComponents({
+          id,
+        }),
+    },
+    "tax-offices": {
+      getDetail: async (id: string) =>
+        await client.taxOffice.getApiCrmServiceTaxOfficesByIdDetail({ id }),
+      get: async (data: { maxResultCount: number; skipCount: number }) =>
+        (await client.taxOffice.getApiCrmServiceTaxOffices(
+          data,
+        )) as GetTaxOfficeDTO,
+
+      deleteRow: async (id: string) =>
+        await client.taxOffice.deleteApiCrmServiceTaxOfficesByIdWithComponents({
+          id,
+        }),
+    },
+    individuals: {
+      getDetail: async (id: string) =>
+        await client.individual.getApiCrmServiceIndividualsById({ id }),
+      get: async (data: { maxResultCount: number; skipCount: number }) =>
+        (await client.individual.getApiCrmServiceIndividuals(
+          data,
+        )) as GetTaxOfficeDTO,
+
+      deleteRow: async (id: string) =>
+        await client.taxOffice.deleteApiCrmServiceTaxOfficesByIdWithComponents({
+          id,
+        }),
+    },
+  };
+
+  return tableRequests;
+}
+
+export type GetTableDataTypes = Awaited<ReturnType<typeof getTableData>>;
+export type GetTableDataResult = Awaited<
+  ReturnType<
+    Awaited<ReturnType<typeof tableDataRequests>>[TableDataTypes]["get"]
+  >
+>;
+export async function getTableData(type: TableDataTypes, page = 0) {
+  try {
+    const requests = await tableDataRequests();
+    return {
+      type: "success",
+      data: await requests[type].get({
+        maxResultCount: 10,
+        skipCount: page * 10,
+      }),
+      status: 200,
+      message: "",
+    };
+  } catch (error) {
+    return structuredError(error);
+  }
+}
+export async function deleteTableRow(type: TableDataTypes, id: string) {
+  try {
+    const requests = await tableDataRequests();
+    return {
+      type: "success",
+      data: await requests[type].deleteRow(id),
+      status: 200,
+      message: "",
+    };
+  } catch (error) {
+    return structuredError(error);
+  }
+}
